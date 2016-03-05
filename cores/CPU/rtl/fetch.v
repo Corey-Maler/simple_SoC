bb807005e073df791c44d0e89466fa9712e40bbf
K.Zarvansky <k.zarvansky@outlook.com>
2016-03-06 01:00:06 +0300

    added registers

--- cores/CPU/rtl/fetch.v
+++ cores/CPU/rtl/fetch.v
@@ -4,8 +4,10 @@ module FETCH(
   input write_mode,
   input [31:0] addr,
   input [31:0] data_i,
+  input [1:0] thread,
   output reg [31:0] data_o,
   output reg ack,
+
 
   input W_CLK,
   input W_ACK,
@@ -15,11 +17,18 @@ module FETCH(
   output reg W_WRITE
 );
 
+
 reg read_from_bus;
-reg state; // 0: 0 -- read input | 1 -- fetch; 1: 00 
+reg [1:0] state; // 0: 0 -- read input | 1 -- fetch; 1: 00 
 reg [1:0] w_state;
 reg w_buff;
 reg w_ack_local;
+
+wire [4:0] reg_select;
+
+assign reg_select = {thread, addr[2:0]}; 
+
+reg [31:0] registers [8 * 4 * 4];
 
 initial
 begin
@@ -29,21 +38,37 @@ end
 always @(posedge clk)
 begin
   if (f_enable)
-  if (state == 0)
-  begin
-    data_o <= 32'd234;
-    read_from_bus <= 1;
-
-    state <= 1;
-  end
+    case (state)
+      2'b00:
+       begin
+        data_o <= 32'd234;
+	case (addr[31:28])
+	  4'b0001:
+	  begin
+	   if (write_mode)
+	     registers[reg_select] <= data_i;
+	   else
+	     data_o <= registers[reg_select];
+	   ack <= 1;
+	  end
+	  4'b1000:
+	  begin
+            read_from_bus <= 1;
+	  end
+	endcase
+        state <= 2'b01;
+       end
+      2'b01: // load from WBUS
+       begin
+         if (w_ack_local)
+         begin
+          data_o <= w_buff;
+          state <= 0;
+        end
+       end
+     endcase
   else
-  begin
-   if (w_ack_local)
-   begin
-     data_o <= w_buff;
-     state <= 0;
-   end
-  end
+    ack <= 0;
 end
 
 always @(posedge W_CLK)
--- cores/CPU/tb/fetch_tb.v
+++ cores/CPU/tb/fetch_tb.v
@@ -4,6 +4,7 @@ reg clk;
 
 reg f_enable;
 reg write_mode;
+reg [1:0] thread;
 reg [31:0] addr;
 reg [31:0] data_i;
 wire ack;
@@ -18,7 +19,7 @@ wire W_WRITE;
 reg W_ACK;
 reg [31:0] W_DATA_I;
 
-FETCH fetch1(clk, f_enable, write_mode, addr, data_i, data_o, ack, W_CLK, W_ACK, W_DATA_I, W_DATA_O, W_ADDR, W_WRITE);
+FETCH fetch1(clk, f_enable, write_mode, addr, data_i, thread, data_o, ack, W_CLK, W_ACK, W_DATA_I, W_DATA_O, W_ADDR, W_WRITE);
 
 
 initial
