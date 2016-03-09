293880140bf869a950b7da5724f4f0ef2eea51f6
K.Zarvansky <k.zarvansky@outlook.com>
2016-03-09 21:38:08 +0300

    full states

--- cores/CPU/rtl/cpu.v
+++ cores/CPU/rtl/cpu.v
@@ -1,15 +1,22 @@
-`define INIT      4'b0
-`define LOAD_INST 4'h1
-`define DEC_INST  4'h2
-`define FETCH_OP1 4'h3
-`define FETCH_OP2 4'h4
-`define FETCH_OP3 4'h5
+`define INIT        5'd0
+`define LOAD_INST_1 5'd1
+`define LOAD_INST_2 5'd2
+`define DEC_INST    5'd3
+`define FETCH_OP1_1 5'd4
+`define FETCH_OP1_2 5'd5
+`define FETCH_OP2_1 5'd6
+`define FETCH_OP2_2 5'd7
+`define FETCH_OP3_1 5'd8
+`define FETCH_OP3_2 5'd9
 // never load Z
-`define LOAD_OP2  4'h6
-`define LOAD_OP3  4'h7
-`define COMPUTE   4'h8
-`define STORE     4'h9
-`define NEXT      4'ha
+`define LOAD_OP2_1  5'd10
+`define LOAD_OP2_2  5'd11
+`define LOAD_OP3_1  5'd12
+`define LOAD_OP3_2  5'd13
+`define COMPUTE     5'd14
+`define STORE_1     5'd15
+`define STORE_2     5'd16
+`define NEXT        5'd17
 
 
 module CPU (
@@ -30,7 +37,9 @@ reg [31:0] CP;
 
 reg skip;
 
-reg f_op1, f_op2, f_op3;
+reg f_op1, f_op2, f_op3, f_store;
+
+reg f_ack;
 
 always @(posedge clk)
 begin
@@ -38,33 +47,44 @@ begin
    `INIT:
      // check interrupts
      state <= `LOAD_INST;  
-   `LOAD_INST:
+   
+   `LOAD_INST_1:
      begin
+     
+       state <= `LOAD_INST_2;
+     end
 
-
+   `LOAD_INST_2:  
+     begin
        state <= `DEC_INST;
      end
+
    `DEC_INST:
      begin
        if (skip):
        begin
          CP <= CP + 32'd4;
-         state <= `LOAD_INST;
+         state <= `INIT;
        end
        else:
        if (f_op1):
-         state <= `FETCH_OP1;
+         state <= `FETCH_OP1_1;
        else:
          state <= `COMPUTE;
      end
-   `FETCH_OP1:
+
+   `FETCH_OP1_1:
+     begin
+       state <= `FETCH_OP1_2;
+     end
+
+   `FETCH_OP1_2:
      begin
        // if FETCH_ACK
        if (f_op2):
          state <= `FETCH_OP2;
        else:
          state <= `LOAD_OP1;
-
      end
    `FETCH_OP2:
      begin
